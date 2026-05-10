d62064ef4e6a9145d2b321e55ce7e87938df79a6
scroopynibbz <connalrobin@gmail.com>
2026-05-10 11:22:46 +0200

    Second attempt at committing Changed SSH key to test

---
+++ Second_Att.R
@@ -0,0 +1,9 @@
+print("This was printed in RStudio")
+
+print("This is the second attempt at a Project")
+
+print("The previous one would not properly 'Push' to the Repository on GitHub")
+
+print("I think it had something to do with the SSH vs. HTTP ")
+
+print("If asking for details, I need to use my PAT to Commit and Push")
