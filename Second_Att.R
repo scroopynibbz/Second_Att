e8f685a492742c7a2b93d8b13041aca3b76230ce
scroopynibbz <connalrobin@gmail.com>
2026-05-10 12:45:33 +0200

    SSH keys should be the same now for this project and what is on GitHUb second repo "Second_Att"

--- Second_Att.R
+++ Second_Att.R
@@ -9,4 +9,3 @@ print("I think it had something to do with the SSH vs. HTTP ")
 print("If asking for details, I need to use my PAT to Commit and Push")
 
 print("Okay, I think I changed the SSH settings, so it should work now")
-
