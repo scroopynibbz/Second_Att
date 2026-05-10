64b3b3d7ddde9a212186ad8dd89f1920c82da2c3
scroopynibbz <connalrobin@gmail.com>
2026-05-10 13:11:30 +0200

    Added GitHub SSH fingerprint to known hosts of ssh connections

--- Second_Att.R
+++ Second_Att.R
@@ -10,3 +10,10 @@ print("If asking for details, I need to use my PAT to Commit and Push")
 
 print("Okay, I think I changed the SSH settings, so it should work now")
 
+print("Okay, so I think it's fixed now. I confirmed the authenticity of the key from GitHub.")
+
+print("I permanently added github.com (ED25519) key fingerprint to the list of known hosts")
+
+print("I used the script: 'ssh -T git@github.com' to add GitHubs finger print to known hosts")
+
+print("After doing this, my edited script was able to be committed to Second_Att repo")
